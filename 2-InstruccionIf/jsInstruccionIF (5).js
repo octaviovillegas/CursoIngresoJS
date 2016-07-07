58168367a7fcff31c8acf2f84f3fd5f8903d9aab
ivanreimer <ivanreimer1@gmail.com>
2016-07-06 22:00:23 -0300

    ULTIMO COMMIT DE LA 3 CLASE

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -19,11 +19,21 @@ else
 		alert("Usted no es adolescente");
 	}
 */
-if(edad>=17)
+if(edad<=17)
 	{
-		if(edad<);
-		{
-
-		}
+	
 	}
+		else
+			{
+				alert("Usted no es adolescente");
+			}	
+				if(edad>=13)
+				{
+
+				}
+else
+	{
+		alert("Usted no es adolescente");
+	}
+		
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -3,7 +3,7 @@ function Mostrar()
 //tomo la edad  
 var edad;
 edad=document.getElementById('edad').value;
-if(edad>17)
+/*if(edad>17)
 	{
 		alert("Usted es mayor de edad");
 	}
@@ -15,4 +15,35 @@ if(edad<13)
 	{
 		alert("Usted es niño");
 	}
+*/
+/*
+if(edad<13)
+	{	
+		alert("Es menor");
+	}
+if(edad<17&&edad>12)
+	{
+		alert("Es adolescente");
+	}
+	else
+		{
+			alert("Es mayor");
+		}
+*/
+if(edad<13)
+	{
+		alert("Es menor");
+	}
+	else
+	{
+		if(edad>18)
+		{
+			alert("Es menor");
+		}
+		else
+		{
+			alert("Es adolescente")
+		}
+	}	
+// ASI ES COMO SE ESCRIBE, PQ OCUPO DOS LUGARES COMO MAXIMO!!!
 }//FIN DE LA FUNCIÓN
