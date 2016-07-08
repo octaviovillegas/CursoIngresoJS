bbbe9f65affef0cc4daea05d76e887c958958c80
IvanReimer <ivanreimer1@gmail.com>
2016-07-08 20:27:06 -0300

    Ejercicios de entrada y salida terminados HASTA EL 7.

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -5,6 +5,5 @@ function Mostar()
 var nombre=document.getElementById('elNombre').value;
 var edad=document.getElementById('laEdad').value;
 alert("Usted se llama "+nombre+" y tiene "+edad+" años.");
-
 }
 
