fd775f7d43758f410fe9e8a2cbdd308a020b8a8f
IvanReimer <ivanreimer1@gmail.com>
2016-07-08 20:22:22 -0300

    Prueba de realizar un commit desde una carpeta clonada.

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,9 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostar()
 {	
-	
+var nombre=document.getElementById('elNombre').value;
+var edad=document.getElementById('laEdad').value;
+alert("Usted se llama "+nombre+" y tiene "+edad+" años.");
+
 }
 
