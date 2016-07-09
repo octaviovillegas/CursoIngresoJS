14a0a68151808bfcac6b80c627380e87832b048c
IvanReimer <ivanreimer1@gmail.com>
2016-07-08 21:17:22 -0300

    Ejercicios de entrada y salida hechos.

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,8 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+var importe=document.getElementById('importe').value;
+var importeNumeroEntero=parseInt(importe);
+var resultado=importeNumeroEntero-importeNumeroEntero*0.25;
+document.getElementById('resultado').value=resultado;
 }
