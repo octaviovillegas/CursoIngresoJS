cf9cad2f9c7526053915502e2a55e35032c7ced2
IvanReimer <ivanreimer1@gmail.com>
2016-07-10 06:16:22 -0300

    Ejercicios 5 y 6 de instruccion switch terminados. Percibo falta de conocimiento de mi parte porque me llevo bastante tiempo terminar el 6. Intuyo que debe haber una funcion para no poner todos los horarios.

--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -2,7 +2,49 @@ function Mostrar()
 {
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
-
-
-
+var horario=parseInt(laHora);
+switch(horario)
+{
+	case 7:
+	case 8:
+	case 9:
+	case 10:
+	case 11:
+	{
+		alert("Es de ma;ana");
+		break;
+	}
+	case 12:
+	case 13:
+	case 14:
+	case 15:
+	case 16:
+	case 17:
+	case 18:
+	case 19:
+	{
+		alert("Es de tarde");
+		break;
+	}
+	case 20:
+	case 21:
+	case 22:
+	case 23:
+	case 24:
+	case 0:
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+	case 6:
+	{
+		alert("Es de noche");
+		break;
+	}
+	default:
+	{
+		alert("La hora no existe");
+	}
+}
 }//FIN DE LA FUNCIÓN
