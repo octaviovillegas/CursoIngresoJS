a37a48424168e541e49a35c2d20e4f2f619d801f
arielskap <Arielskap@gmail.com>
2017-02-01 12:32:56 -0300

    adads

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,8 +2,8 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	var valor= <input type="text"  placeholder="Su nombre" id="elNombre">
-	alert(valor)
+	var valor= document.getelemntbyid("").value
+	alert(Valor)
 }
 
 
