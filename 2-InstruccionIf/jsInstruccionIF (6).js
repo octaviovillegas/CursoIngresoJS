e5b5ced174f10d67666232fab8ff6db0c199222d
alumno <alumno>
2017-02-02 12:28:42 -0300

    Programacion

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,18 @@
 function Mostrar()
 {
 //tomo la edad  
-
-
-
-
+var años;
+años=document.getElementById('edad').value;
+if (años>=18) 
+{
+	console.log("Mayor de edad")
+}
+else (años>=13 && años<=17)
+{
+	console.log("Adolescente")
+}
+else (años<13)
+{
+	console.log("Niño")
+}
 }//FIN DE LA FUNCIÓN
