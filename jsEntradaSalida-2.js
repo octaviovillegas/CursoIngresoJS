3dfb3349c2ff06a22ea0e413962204b41cd539d2
Ariellago <lagoariel@gmail.com>
2017-02-02 20:27:33 -0300

    PRIMER COMMIT
    Hicimos los Ejercicios 1 y 2 en clase.

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,11 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var nombre = "Ariel";
+
+
+	nombre = prompt("Ingrese su nombre");
+	alert(nombre);
+}
+
