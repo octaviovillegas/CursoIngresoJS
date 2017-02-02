8aaa8d45b050e676a8b9f92337e02cfb4808943c
Zinad <leandrosineriz@gmail.com>
2017-02-02 05:10:51 -0300

    day 1

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -2,7 +2,13 @@
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
+var num1;
 function MostrarAumento()
 {
-	
+	num1 = document.getElementById("importe").value;
+    var importe = parseInt(num1);
+    var descuento = (importe * 25)/100;
+    var nuevoImporte = importe - descuento;
+    
+    document.getElementById("resultado").value=nuevoImporte;
 }
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -2,23 +2,46 @@
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+var num1;
+var num2;
 function sumar()
 {	
+    num1 = document.getElementById("numeroUno").value;
+    num2 = document.getElementById("numeroDos").value;
+    var x = parseInt(num1);
+    var y = parseInt(num2);
+    var suma = x + y;
+    alert("la suma es " + suma);
 	
 }
 
 function restar()
 {
-	
+	num1 = document.getElementById("numeroUno").value;
+    num2 = document.getElementById("numeroDos").value;
+    var x = parseInt(num1);
+    var y = parseInt(num2);
+    var resta = x - y;
+    alert("la resta es " + resta);
 }
 
 function multiplicar()
 { 
-	
+	num1 = document.getElementById("numeroUno").value;
+    num2 = document.getElementById("numeroDos").value;
+    var x = parseInt(num1);
+    var y = parseInt(num2);
+    var multi = x * y;
+    alert("la multiplicacion es " + multi);
 }
 
 function dividir()
 {
-	
+	num1 = document.getElementById("numeroUno").value;
+    num2 = document.getElementById("numeroDos").value;
+    var x = parseInt(num1);
+    var y = parseInt(num2);
+    var div = x / y;
+    alert("la division es " + div);
 }
 
