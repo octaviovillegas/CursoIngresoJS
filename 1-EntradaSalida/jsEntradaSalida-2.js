5f733db1aa10d48e6d882b3315f989d625f01801
alumno <alumno>
2017-02-01 22:24:44 -0300

    ejercios 1 2 3

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,9 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
-{
+{ var   nombre;
+    nombre  = prompt    ("mario")
+    alert   (nombre);
 	
 }
 
