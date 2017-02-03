863a60b023fdfdd2790817bef0dbaa130cc4c2f7
Ariellago <lagoariel@gmail.com>
2017-02-03 20:16:28 -0300

    COMMIT SWITCH EJERCICIOS 3 AL 5

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,16 @@
 function Mostrar()
 {
-//tomo la edad  
+//tomo el mes  
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
+switch(mesDelAño){
+	case 'Febrero':
+		alert("Este mes no tiene mas de 29 de días");
+		break;
+	default:
+		alert("Este mes tiene 30 o más días");
+		break;
+}
 	
-	
-
-
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,7 +4,20 @@ function Mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
+switch(mesDelAño){
+	default:
+		alert("Este mes tiene 31 días");
+		break;
+	case 'Febrero':
+		alert("Este mes tiene 28 o 29 días si es año bisiesto");
+		break;
+	case 'Abril':
+	case 'Junio':
+	case 'Septiembre':
+	case 'Noviembre':
+		alert("Este mes tiene 30 días");
+		break;
+	}
 	
 
 
