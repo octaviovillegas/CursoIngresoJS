cd9fac1cb8e00ad7ece84811914316159e27e686
Ariellago <lagoariel@gmail.com>
2017-02-03 19:32:24 -0300

    COMMIT SWITCH EJERCICIO 2
    falta, estamos o ya pasó el invierno

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -2,8 +2,25 @@ function Mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
-
-alert (mesDelAño);
+switch(mesDelAño){
+		case 'Enero':
+		case 'Febrero':
+		case 'Marzo':
+		case 'Junio':
+			alert('Falta para el invierno');
+			break;
+		case 'Julio':
+		case 'Agosto': 
+			alert("Abrigate que hace frio");
+			break;
+		case 'Septiembre':
+		case 'Octubre':
+		case 'Noviembre':
+		case 'Diciembre':
+			alert("Ya pasamos el frio, ahora calor!!");
+			break;
+}
+//alert (mesDelAño);
 
 
 
