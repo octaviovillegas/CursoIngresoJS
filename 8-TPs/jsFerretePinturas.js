91ae5dc8988b762a94f32704a0c3b5f5ee6c519f
Ariel <lagoariel@gmail.com>
2017-02-04 13:02:01 -0300

    TRABAJO PRACTICO 3

--- 8-TPs/jsFerretePinturas.js
+++ 8-TPs/jsFerretePinturas.js
@@ -2,12 +2,15 @@
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
+var temp;
 function FahrenheitCentigrados () 
 {
-	
+	temp = parseFloat(document.getElementById("Temperatura").value);
+	alert(temp + " Fahrenheit son " + ((temp - 32)/1.8) + " Centígrados");
 }
 
 function CentigradosFahrenheit () 
 {
-	
+	temp = parseFloat(document.getElementById("Temperatura").value);
+	alert(temp + " Centígrados son " + ((temp * 1.8) + 32) + " Fahrenheit");
 }
