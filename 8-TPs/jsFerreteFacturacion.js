b4e181bb1a2a11fe9a61471618a013b0d5d454f5
Ariel <lagoariel@gmail.com>
2017-02-06 09:10:08 -0300

    TP's del 1 al 3

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,28 +3,15 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
-var PrecioUno;
-var PrecioDos;
-var PrecioTres;
-
 function Sumar () 
 {
-	PrecioTres = parseInt(document.getElementById('PrecioTres').value);
-	PrecioDos = parseInt(document.getElementById('PrecioDos').value);
-	PrecioUno = parseInt(document.getElementById('PrecioUno').value);
-	alert(PrecioTres + PrecioDos + PrecioUno);	
+	
 }
 function Promedio () 
 {
-	PrecioTres = parseInt(document.getElementById('PrecioTres').value);
-	PrecioDos = parseInt(document.getElementById('PrecioDos').value);
-	PrecioUno = parseInt(document.getElementById('PrecioUno').value);
-	alert((PrecioTres + PrecioDos + PrecioUno)/3);		
+	
 }
 function PrecioFinal () 
 {
-	PrecioTres = parseInt(document.getElementById('PrecioTres').value);
-	PrecioDos = parseInt(document.getElementById('PrecioDos').value);
-	PrecioUno = parseInt(document.getElementById('PrecioUno').value);
-	alert((PrecioTres + PrecioDos + PrecioUno)*1.21);
+	
 }
