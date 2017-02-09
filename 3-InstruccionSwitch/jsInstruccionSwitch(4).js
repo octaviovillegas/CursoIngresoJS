1047b7ab737aec60b1a7a696753358d72d599aee
Zinad <leandrosineriz@gmail.com>
2017-02-09 07:13:52 -0300

    switch change

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,9 +4,58 @@ function Mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
-	
+	switch(mesDelAño){
+	case "Enero":{
+		console.log("tiene 31 dias");
+		break;
+	}
+	case "Febrero":{
+		console.log("tiene 28 dias");
+		break;
+	}
+	case "Marzo":{
+		console.log("tiene 31 dias");
+		break;
+	}
+	case "Abril":{
+		console.log("tiene 30 dias");
+		break;
+	}
+	case "Mayo":{
+		console.log("tiene 31 dias");
+		break;
+	}
+	case "Junio":{
+		console.log("tiene 30 dias");
+		break;
+	}
+	case "Julio":{
+		console.log("tiene 31 dias");
+		break;
+	}
+	case "Agosto":{
+		console.log("tiene 31 dias");
+		break;
+	}
+	case "Septiembre":{
+		console.log("tiene 30 dias");
+		break;
+	}
+	case "Octubre":{
+		console.log("tiene 31 dias");
+		break;
+	}
+	case "Noviembre":{
+		console.log("tiene 30 dias");
+		break;
+	}
+	case "Diciembre":{
+		console.log("tiene 31 dias");
+		break;
+    }
 
-
+	default:
+	   console.log("no especificado");
+	}
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -3,6 +3,116 @@ function Mostrar()
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
 
+switch(laHora){
+	
+    case "7":{
+		console.log("Es de mañana.");
+		break;
+	}
+	case "8":{
+		console.log("Es de mañana.");
+		break;
+	}
+	case "9":{
+		console.log("Es de mañana.");
+		break;
+	}
+	case "10":{
+		console.log("Es de mañana.");
+		break;
+	}
+	case "11":{
+		console.log("Es de mañana.");
+		break;
+	}
+	case "12":{
+		console.log("Es de tarde.");
+		break;
+	}
+	case "13":{
+		console.log("Es de tarde.");
+		break;
+	}
+	case "14":{
+		console.log("Es de tarde.");
+		break;
+	}
+	case "15":{
+		console.log("Es de mañana.");
+		break;
+	}
+	case "16":{
+		console.log("Es de tarde.");
+		break;
+	}case "17":{
+		console.log("Es de tarde.");
+		break;
+	}
+	case "18":{
+		console.log("Es de tarde.");
+		break;
+	}
+	case "19":{
+		console.log("Es de tarde.");
+		break;
+	}
+	case "20":{
+		console.log("Es de noche.");
+		break;
+	}
+	case "21":{
+		console.log("Es de noche.");
+		break;
+	}
+    case "22":{
+		console.log("Es de noche.");
+		break;
+	}
+	case "23":{
+		console.log("Es de noche.");
+		break;
+	}
+    case "24":{
+		console.log("Es de noche.");
+		break;
+	}
+    case "1":{
+		console.log("Es de noche.");
+		break;
+	}
+    case "2":{
+		console.log("Es de noche.");
+		break;
+	}
+    case "3":{
+		console.log("Es de noche.");
+		break;
+	}
+    case "4":{
+		console.log("Es de noche.");
+		break;
+	}
+    case "5":{
+		console.log("Es de noche.");
+		break;
+	}
+    case "6":{
+		console.log("Es de noche.");
+		break;
+	}
+    case "0":{
+		console.log("Es de noche.");
+		break;
+	}    
+	
+	default:
+	   if(laHora > "24"){
+           console.log("la hora no existe");
+       } else if(laHora < 0){
+           console.log("la hora no existe");
+       }
+        
+	}
 
 
 }//FIN DE LA FUNCIÓN
