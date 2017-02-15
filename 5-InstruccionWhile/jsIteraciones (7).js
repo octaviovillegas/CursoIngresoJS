2ebd1863ae20829a8f20cd8d6f30fd73ae58f549
Ariel <lagoariel@gmail.com>
2017-02-15 12:13:47 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,17 +4,29 @@ function Mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var ingreso;
 
 	while(respuesta == "si"){
-		var ingreso = parseInt(prompt("Ingrese el número a sumar."));
-		//OMITE UN VALOR INGRESADO QUE NO SEA DE TIPO Number
-		if (!isNaN(ingreso))acumulador += ingreso; 
-		contador++;
-		respuesta = prompt("¿Desea seguir sumando? (si/no)");
-		while(!(respuesta == "si" || respuesta == "no")){
-			respuesta = prompt("¿Desea seguir sumando? (si/no)");
+		ingreso = prompt("Ingrese el número a sumar.");
+		ingreso = parseInt(ingreso);
+		//ITERA HASTA QUE INGRESE UN VALOR VALIDO
+		while(isNaN(ingreso)){
+			ingreso = prompt("Ingrese el número a sumar.");
+			ingreso = parseInt(ingreso);
 		}
-	}
+		//suma todos los numeros ingresados
+		acumulador += ingreso;
+		//OMITE UN VALOR INGRESADO QUE NO SEA DE TIPO Number
+		//if (!isNaN(ingreso))acumulador += ingreso; 
+		contador++;
+		
+		if(contador > 1){
+			respuesta = prompt("¿Desea seguir sumando? (si/no)");
+			while(!(respuesta == "si" || respuesta == "no")){
+				respuesta = prompt("Opcion incorrecto, ¿desea seguir sumando? (si/no)");
+			}
+		}
+	}//cierra el while
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
