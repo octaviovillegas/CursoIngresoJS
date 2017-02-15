2f9fc2777dd85c0c8f3993b674ffd765dcb5f694
JonatanValusek <jonatan.valusek@gmail.com>
2017-02-15 17:41:13 -0300

    Ejercicios Switch terminados.

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,12 +1,23 @@
 function Mostrar()
 {
-//tomo la edad  
-var laHora = document.getElementById('hora').value;
 
 //alert (laHora);
 	
-	
+	var laHora = document.getElementById('hora').value;
 
+switch (laHora){
 
+    case "7":
+    case "8":
+    case "9":
+    case "10":
+    case "11":
+   
+    alert ("Es de mañana")
+
+    break;
+
+    default:
+}
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -1,8 +1,49 @@
 function Mostrar()
 {
-//tomo la edad  
+//tomo la hora  
+
 var laHora = document.getElementById('hora').value;
 
+switch (laHora){
 
+    case "7":
+    case "8":
+    case "9":
+    case "10":
+    case "11":
+    alert ("Es de mañana")
+    break;
 
-}//FIN DE LA FUNCIÓN
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "20":
+    case "21":
+    case "22":
+    case "23":
+    case "24":
+    alert ("Es de Noche")
+    break;
+   
+    case "12":
+    case "13":
+    case "14":
+    case "15":
+    case "16":
+    case "17":
+    case "18":
+    case "19": 
+   
+    alert ("Es de tarde")
+    break;
+
+    default:
+    alert ("La hora no existe")
+}
+
+}
+//FIN DE LA FUNCIÓN
