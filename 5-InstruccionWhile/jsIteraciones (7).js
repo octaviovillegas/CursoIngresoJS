19d3167e8aaf1ecd788bdb55df95f79d14be63b4
alumno <alumno>
2017-02-15 22:12:02 -0300

    df

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,8 +3,12 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var respuesta="si";
 
+while ( respuesta != "si"){
+ respuesta
+
+}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
