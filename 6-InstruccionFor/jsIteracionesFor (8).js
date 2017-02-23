e352e42d458df294ecef95c2d59eed53eb763d49
alumno <alumno>
2017-02-22 22:13:19 -0300

    tp

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -2,8 +2,11 @@ function Mostrar()
 {
 
 var  num;
+while(isNaN(num)){
+
 num = prompt("ingrese Un numero");
 num=parseInt(num)
+}
 var contDiv=0;
 
 for(var i=1 ; i<=num ; i++)
@@ -11,12 +14,10 @@ for(var i=1 ; i<=num ; i++)
     if(num%i==0){
     contDiv++;}
 }
-if (contDiv==2){
+if (contDiv<=2){
  console.log(num+" es un Numero Primo");
 }
 else {
     console.log(num+" no es un numero primo")
 }
-
-
 }//FIN DE LA FUNCIÓN
