84418d01bf1a6f2a028002a551c493b6c76caf1a
alumno <alumno>
2019-02-06 22:13:17 -0300

    ejercicio 4

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,19 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+	var sumar;
+	var multiplicar;
+    var resultado;
+
+sumar = numeroDivisor.value;
+multiplicar = numeroDividendo.value;
+resultado = SacarResto.value;
+
+
+
+
+
+
+
 }
+
