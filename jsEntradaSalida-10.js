fa881dfe42802921ee020db94876b43b28c4f2f4
lautarosubeldia <47337541+lautarosubeldia@users.noreply.github.com>
2019-02-11 10:10:33 -0300

    Consultar/Resolver
    No lo toma

--- jsEntradaSalida-10.js
+++ jsEntradaSalida-10.js
@@ -4,5 +4,24 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+
 	
+	var sueldo;
+	var sueldoConDescuento;
+	var descuento;
+
+
+		sueldo = document.getElementById('sueldo').value;
+		sueldo = parseFloat(sueldo);
+
+
+			descuento = sueldo * 0.25;
+
+
+				sueldoConDescuento = sueldo + descuento;
+					
+
+		sueldoConDescuento = document.getElementById('resultado').value;
+
+		
 }
