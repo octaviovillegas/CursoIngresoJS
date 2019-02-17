5e14c861675aec7137408ddc8b3b90255563c00f
lautarosubeldia <47337541+lautarosubeldia@users.noreply.github.com>
2019-02-17 20:08:30 -0300

    ok 17/2 II

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -14,19 +14,104 @@ var contadorIntentos;
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
+
 	numeroSecreto = Math.floor((Math.random() * 100) +1);
 
 	console.log(numeroSecreto);
 
 	contadorIntentos = 0;
 
-
 }
 
 function verificar()
 {
+
+	var numeroIngresado;
+
+	var numeroIntento;
+
+	numeroIngresado = numero.value;
 	
+	numeroIntento = intentos.value; 
+
+	contadorIntentos = contadorIntentos + 1;
+
+	if (contadorIntentos == 1 &&  numeroIngresado == numeroSecreto) 
+	{
+		alert("usted es un Psíquico");
+	}
+	 if (contadorIntentos == 2 &&  numeroIngresado == numeroSecreto) 
+	{
+		alert("excelente percepción");
+	}
+	 if (contadorIntentos == 3 &&  numeroIngresado == numeroSecreto) 
+	{
+		alert("Esto es suerte");
+	}
+	 if (contadorIntentos == 4 &&  numeroIngresado == numeroSecreto) 
+	{
+		alert("Excelente técnica");
+	}
+	 if (contadorIntentos == 5 &&  numeroIngresado == numeroSecreto) 
+	{
+		alert("usted está en la media");
+	}
+	if (contadorIntentos > 5 && contadorIntentos < 10 &&numeroIngresado == numeroSecreto) 
+	{
+		alert("falta técnica");
+	}
+	if (contadorIntentos > 10 &&  numeroIngresado == numeroSecreto) 
+	{
+		alert("afortunado en el amor!!");
+	}
 	
+
+
+
+
+/*"Ingrese número" id="numero">
+readonly id="intentos">*/
+
+/*En esta oportunidad el juego evaluará tus aptitudes a partir de la cantidad de intentos, por lo cual se informará los siguientes: 
+1° intento: “usted es un Psíquico”. 
+2° intento: “excelente percepción”. 
+3° intento: “Esto es suerte”. 
+4° intento: “Excelente técnica”. 
+5° intento: “usted está en la media”. 
+Desde 6 Intentos hasta 10:”falta técnica” 
+Más de 10 intentos: “afortunado en el amor!!”.*/ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -1,29 +1,81 @@
-/*Al comenzar el juego generaremos un número 
-RANDOM del 1 al 3 para la selección de la máquina, 
-siendo 1 para “piedra”, el 2 para “papel” y 3 para “tijera”.
-El jugador seleccionará una imagen correspondiente 
-a su opción  y le informaremos si ganó, empató o perdió.
+/*
+Al comenzar el juego generaremos un número 
+1	RANDOM del 1 al 3 para la selección de la máquina, 
+2	siendo 1 para “piedra”, el 2 para “papel” y 3 para “tijera”.
+3	El jugador seleccionará una imagen correspondiente a su opción  y 
+4	le informaremos si ganó, empató o perdió.
 */
 var eleccionMaquina;
 
 function comenzar()
 {
-	
 
+	eleccionMaquina = Math.floor((Math.random() * 3) + 1);
 
+	//alert(eleccionMaquina);
+		
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
+
+	piedra = 1;
+	papel = 2;
+	tijera = 3;
 	
+	if (eleccionMaquina == piedra) 
+	{
+		alert("empató");
+	}
+	else if (eleccionMaquina == tijera) 
+	{
+		alert("ganó");
+	}
+	else if (eleccionMaquina == papel) 
+	{
+		alert("perdió");
+	}
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
 
+	piedra = 1;
+	papel = 2;
+	tijera = 3;
+	
+	if (eleccionMaquina == papel) 
+	{
+		alert("empató");
+	}
+	else if (eleccionMaquina == piedra) 
+	{
+		alert("ganó");
+	}
+	else if (eleccionMaquina == tijera) 
+	{
+		alert("perdió");
+	}
+
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
+
+	piedra = 1;
+	papel = 2;
+	tijera = 3;
+
+	if (eleccionMaquina == tijera) 
+	{
+		alert("empató");
+	}
+	else if (eleccionMaquina == papel) 
+	{
+		alert("ganó");
+	}
+	else if (eleccionMaquina == piedra) 
+	{
+		alert("perdió");
+	}
 
 }//FIN DE LA FUNCIÓN
