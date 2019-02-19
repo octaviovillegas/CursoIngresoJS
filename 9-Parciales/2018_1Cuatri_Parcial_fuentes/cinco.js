0a3ba2f3f42ac2c934a5174ed68207e0f2387f10
lautarosubeldia <47337541+lautarosubeldia@users.noreply.github.com>
2019-02-19 15:47:54 -0300

    19/2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,58 @@
 function mostrar()
 {
+var Planetas;
+//var mensaje;
+
+Planetas = document.getElementById('Planetas')
+
+Planetas = prompt("Favor de colocar planetas que se encuentren en el sistema solar");
+
+switch (Planetas) 
+{
+
+	case "Mercurio":
+	case "Venus":
+	case "Marte":
+		alert("acá hace más calor");
+		break;
+	case "Tierra":
+		alert( "acá vivimos");
+		break;
+	case "Marte":
+	case "Júpiter":
+	case "Saturno":
+	case "Urano":
+	case "Neptuno":
+		alert("acá hace más frio");
+		break;
+	default:
+		alert("no es válido " + Planetas);
+		break;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
@@ -1,4 +1,38 @@
 function mostrar()
 {
+var hora;
+
+hora = document.getElementById('laHora');
+
+if (hora > 5 && hora < 12) 
+{
+ alert("es de mañana"); 
+ else
+ 	if (hora > 11 && hora <19) 
+ 	{
+ 		alert("es de tarde");
+ 	}//hora > 11 && hora <19
+}//hora > 5 && hora < 12
+else
+{
+	alert("es de noche");
+}
+
+
+
+
+
+
+
+
+
+
+
 
 }
+/*switch (true) 
+{
+	case (laHora > 5 && laHora < 12):
+		alert("es de mañana");
+		break;
+}*/
