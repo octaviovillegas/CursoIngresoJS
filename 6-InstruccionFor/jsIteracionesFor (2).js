e439e7ea98045014be8c5fa0c4cbe99db837f70d
lautarosubeldia <47337541+lautarosubeldia@users.noreply.github.com>
2019-02-27 21:02:09 -0300

    27/2

--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,90 @@
 function mostrar()
 {
+	var numeroIngresado;
+	var numeroAnterior;
+
+	numeroIngresado = prompt(numeroIngresado);
+
+	numeroIngresado = parseInt(numeroIngresado);
+
+
+	/*for(numeroAnterior = numeroIngresado - 1; numeroAnterior > 1; numeroAnterior --) 
+	{
+
+		console.log(numeroAnterior);
+
+		if (numeroIngresado % numeroAnterior == 0) 
+		{
+			console.log("es primo");
+			
+			break;
+		}
+
+	}
+
+	if(numeroAnterior == 1) 
+	{
+		console.log("es primo"); 
+		
+		else 
+		{
+			alert("no es primp");
+				//exprime
+		}
+	}*/
+
+	for (recorrido = numeroIngresado - 1; recorrido > 1 ; recorrido -- ) 
+	{
+		for(numeroAnterior = recorrido - 1; numeroAnterior > 1; numeroAnterior --) 
+		{
+
+			console.log(numeroAnterior);
+
+			if (numeroIngresado % numeroAnterior == 0) 
+			{
+				break;
+			}
+
+		}
+
+		if(numeroAnterior == 1) 
+		{
+			console.log("es primo" + recorrido); 
+			
+			/*else if (numeroAnterior != 1) 
+			{
+				console.log("no es primo");
+					//exprime
+			}*/
+
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
--- 6-InstruccionFor/jsIteracionesFor (4).js
+++ 6-InstruccionFor/jsIteracionesFor (4).js
@@ -1,7 +1,9 @@
 function mostrar()
+
+/*todos los numero ant es la suma de los que le siguen, ventros nùmericos, 
+programa-me.com/2015/reg/ourense/marcador/public/programaMe-Regional-RegionalOurense2015-E.pdf
+*/
 {
-
-
 
 
 }//FIN DE LA FUNCIÓN
