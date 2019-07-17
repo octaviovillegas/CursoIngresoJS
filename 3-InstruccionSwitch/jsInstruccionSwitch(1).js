9bd109e6c279b0249b154a66422398edb2d1438f
CRISTIANROMANO23 <50123714+CRISTIANROMANO23@users.noreply.github.com>
2019-07-16 21:52:26 -0300

    Add files via upload
    ABBB

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,7 +3,21 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+switch (mesDelAño){
+    case "Enero": 
+    alert ("que comiences bien el año!!!.");
+    break;
+    
+    case "Marzo": 
+    alert ("a clases!!!.");
+    break;
 
+    case "Julio": 
+    alert ("se vienen las vacaciones!!.");
+    break;
 
-
+    case "Diciembre": 
+    alert ("Felices fiesta!!!.");
+    break;
+}
 }//FIN DE LA FUNCIÓN
