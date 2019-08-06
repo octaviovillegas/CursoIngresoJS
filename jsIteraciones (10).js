916db89e0c6e2d668f9c4adbd7d5d23f5209e1e0
CRISTIANROMANO23 <50123714+CRISTIANROMANO23@users.noreply.github.com>
2019-08-05 22:08:48 -0300

    while ej 9 - 10
    ...

--- jsIteraciones (10).js
+++ jsIteraciones (10).js
@@ -1,16 +1,62 @@
 function mostrar()
 {
 
-	var contador=0;
-	//declarar contadores y variables 
-	
-	var respuesta="si";
+var numero;
+var numeroP = 0;
+var numeroN = 0;
+var contadorP = 0;
+var contadorN = 0;
+var contadorC = 0;
+var contadorPar = 0;
+var promedioP = 0;
+var promedioN = 0;
+var diferencia;
 
-	while(respuesta!="no")
-	{
-		
-	
+
+
+do{
+	numero = parseInt(prompt("ingrese numero"));
+	while(isNaN(numero)){
+		numero = parseInt(prompt("No es un numero , ingrese numero"));
 	}
+
+	if(numero > 0){
+		numeroP = numero + numeroP;
+		contadorP = contadorP + 1;
+
+	}
+	
+	else if (numero < 0 ){
+		numeroN = numero + numeroN;
+		contadorN = contadorN + 1;
+	}
+
+	else{
+		contadorC = contadorC +1;
+	}
+
+	if(numero %2==0){
+		contadorPar = contadorPar + 1;
+
+	}
+
+}while(confirm("desea ingresar otro numero?"));
+
+
+		promedioP = numeroP / contadorP;
+		promedioN = numeroN / contadorN;
+		diferencia = numeroP - numeroN;
+	
+
+	document.write("la suma de numeros positivos da: " + numeroP + "<br>");
+	document.write("la suma de numeros negativos da: " + numeroN + "<br>");
+	document.write("cantidad de positivos: " + contadorP + "<br>");
+	document.write("cantidad de negativos: " + contadorN + "<br>");
+	document.write("cantidad de ceros: " + contadorC + "<br>");
+	document.write("cantidad de pares: " + contadorPar + "<br>");
+	document.write("promedio de positivos es: " + promedioP + "<br>");
+	document.write("promedio de negativos es: " + promedioN + "<br>");
+	document.write("la diferencia es: " + diferencia + "<br>");
 
 
 
--- jsIteraciones (9).js
+++ jsIteraciones (9).js
@@ -1,18 +1,31 @@
 function mostrar()
 {
 
-	var contador=0;
-	// declarar variables
-	
-	var respuesta='si';
+var num;
+var maximo;
+var minimo;
+var flag = 0;
 
-	while(respuesta!='no')
-	{
-		
+
+do{
+	num = parseInt(prompt("INGRESE NUMERO"));
+	while(isNaN(num)){
+		num = parseInt(prompt("NO ES NUMERO , INGRESE NUMERO"));
+	}
+
+	if(num > maximo || flag == 0 ){
+		maximo = num + maximo;}
+
+	if(num < minimo || flag == 0){
+		minimo = num + minimo;
 	
 	}
 
+	flag = 1;
 
+}while(confirm("DESEA INGRESAR OTRO NUMERO?"));
 
+document.getElementById("maximo").value = maximo;
+document.getElementById("minimo").value = minimo;
 
 }//FIN DE LA FUNCIÓN
