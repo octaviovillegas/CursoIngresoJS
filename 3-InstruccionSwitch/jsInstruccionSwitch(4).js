8605561eb12842c3f828c9eaab1ed4e689e67671
CRISTIANROMANO23 <50123714+CRISTIANROMANO23@users.noreply.github.com>
2019-08-09 16:37:27 -0300

    NO VASELINE

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,27 @@
 function mostrar()
 {
-//tomo la edad  
+//tomo la edad
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
-	
 
+switch(mesDelAño){
+	case 'Febrero':
+	alert('este mes no tiene mas de 28 dias');
+	break;
+
+	case 'Abril':
+	case 'Julio':
+	case 'Septiembre':
+	case 'Noviembre':
+	alert('tiene 30 dias');
+	break;
+
+	default:
+	alert('tiene 31 dias');
+	break;
+
+}
 
 
 }//FIN DE LA FUNCIÓN
