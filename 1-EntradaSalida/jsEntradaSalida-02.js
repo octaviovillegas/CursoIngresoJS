bed690ddb7f0d5e3c5daf099e7aa81cadbe3e77e
GonvoDS <gon.mat.d.s@gmail.com>
2021-07-06 11:58:23 -0300

    Primera clase

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,15 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
-function mostrar()
-{
-	alert("ok");
+function mostrar() {
+	let dato;
+
+	dato = prompt("Nombre")
+
+
+
+	alert("Su nombre es " + dato);	
+
+
 }
 
