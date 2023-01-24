01e9c5fa0116e90b488bc05585adf496e6388634
Daiana3010 <idkfa9512@gmail.com>
2023-01-24 18:44:59 -0300

    tengo calor

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,7 +3,7 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo DAIANA");
-	
+	alert("Hola Mundo soy DAIANA");
+
 }
 
