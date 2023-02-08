a0ff439754c03865dc4d812d129bed328bc6747b
angel-farina <zzyzxxrd@gmail.com>
2023-02-08 14:22:14 -0300

    updated

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -6,13 +6,15 @@ function Sumar ()
 	var p2;
 	var p3;
     var res;
+    var mensaje;
 
     p1=txtIdPrecioUno.value;
     p2=txtIdPrecioDos.value;
     p3=txtIdPrecioTres.value;
     res=parseInt(p1)+parseInt(p2)+parseInt(p3);
+    mensaje="El resultado es " + res;
 
-    alert("El resultado es " + res);	
+    alert(mensaje);	
 }
 function Promedio () 
 {
@@ -21,14 +23,16 @@ function Promedio ()
 	var p3;
     var res;
     var res2;
+    var mensaje;
 
     p1=txtIdPrecioUno.value;
     p2=txtIdPrecioDos.value;
     p3=txtIdPrecioTres.value;
     res=parseInt(p1)+parseInt(p2)+parseInt(p3);
     res2=res/3;
+    mensaje="El resultado es " + res2;
 
-    alert("El resultado es " + res2);
+    alert(mensaje);
 }
 function PrecioFinal () 
 {
@@ -37,12 +41,14 @@ function PrecioFinal ()
 	var p3;
     var res;
     var res2;
+    var mensaje;
 
     p1=txtIdPrecioUno.value;
     p2=txtIdPrecioDos.value;
     p3=txtIdPrecioTres.value;
     res=parseInt(p1)+parseInt(p2)+parseInt(p3);
     res2=(res*21)/100;
+    mensaje="El resultado es " + res2;
 
-    alert("El resultado es " + res2);
+    alert(mensaje);
 }
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -5,22 +5,26 @@ function Rectangulo ()
     var largo;
     var ancho;
     var res;
+    var mensaje;
 
     largo=txtIdLargo.value;
     ancho=txtIdAncho.value;
     res=parseInt(largo*2)+parseInt(ancho*2)*3/3;
+    mensaje=`Se debe comprar ${res} cantidad de alambre`
 
-    alert(`Se debe comprar ${res} cantidad de alambre`);
+    alert(mensaje);
 }
 function Circulo () 
 {
 	var radio;
     var res;
+    var mensaje;
 
     radio=txtIdRadio.value;
     res=(2*3.14*parseInt(radio))*3/3;
+    mensaje=`Se debe comprar ${res} cantidad de alambre`;
 
-    alert(`Se debe comprar ${res} cantidad de alambre`);
+    alert(mensaje);
 }
 function Materiales () 
 {
@@ -29,12 +33,14 @@ function Materiales ()
     var area;
     var cemento;
     var cal;
+    var mensaje;
 
     largo=txtIdLargo.value;
     ancho=txtIdAncho.value;
     area=parseInt(largo)*parseInt(ancho);
     cemento=area*2
     cal=area*3
-    
-    alert(`Se necesitan ${cemento} bolsas de cemento y ${cal} bolsas de cal`);
+    mensaje=`Se necesitan ${cemento} bolsas de cemento y ${cal} bolsas de cal`;
+
+    alert(mensaje);
 }
